6510def51d41855998074d3801d15fbe9dad74e6
pratik kad <pratikkad22@gmail.com>
2025-03-01 22:48:52 +0530

    Add restaurant form updated

--- src/components/AddRestaurant.jsx
+++ src/components/AddRestaurant.jsx
@@ -1,69 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
 function AddRestaurant() {
-    const navigate=useNavigate();
-    function backClickHandler() {
-        navigate(-1);
-    }
+  const navigate = useNavigate();
+  function backClickHandler() {
+    navigate(-1);
+  }
+
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    city: "",
+    status: "",
+    image: "",
+  });
+
+  function changeHandler(event) {
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    console.log("form sumbit details", formData);
+    navigate("/restaurants");
+  }
 
   return (
     <div className="mt-24 w-1/2 mx-auto text-zinc-50 bg-[#1A1A1AB3] p-6 rounded-md">
-      <div onClick={backClickHandler} className="h-10 w-10 bg-[#1A1A1ACC] hover:bg-zinc-900 rounded-full flex justify-center items-center">
+      <div
+        onClick={backClickHandler}
+        className="h-10 w-10 bg-[#1A1A1ACC] hover:bg-zinc-900 rounded-full flex justify-center items-center"
+      >
         <IoMdArrowRoundBack size={22} />
       </div>
       <h1 className="my-4 text-2xl font-bold ">Add new restaurant</h1>
-      <form action="">
+      <form onSubmit={formSubmitHandler} action="">
         <div className="flex flex-col gap-2">
           <label className="font-semibold" htmlFor="name">
             Name of Restaurant
           </label>
           <input
+            onChange={changeHandler}
             className="border-1 p-1 rounded-md"
             type="text"
             placeholder="Name of Restaurant"
             id="name"
+            name="name"
           />
 
           <label className="font-semibold" htmlFor="address">
             Address
           </label>
           <input
+            onChange={changeHandler}
             className="border-1 p-1 rounded-md"
             type="text"
             placeholder="Enter address"
             id="address"
+            name="address"
           />
 
           <label className="font-semibold" htmlFor="city">
             City
           </label>
           <input
+            onChange={changeHandler}
             className="border-1 p-1 rounded-md"
             type="text"
             placeholder="Enter city"
             id="city"
+            name="city"
           />
 
           <label className="font-semibold" htmlFor="status">
             Currest status
           </label>
           <input
+            onChange={changeHandler}
             className="border-1 p-1 rounded-md"
             type="text"
             placeholder="open / closed"
             id="status"
+            name="status"
           />
 
           <label className="font-semibold" htmlFor="image-url">
             URL
           </label>
           <input
+            onChange={changeHandler}
             className="border-1 p-1 rounded-md"
             type="link"
             placeholder="Enter image URL"
             id="image-url"
+            name="image"
           />
 
           <button className=" py-2 font-semibold text-sm bg-blue-400 hover:bg-blue-300 rounded-md mt-2">
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,75 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
 
-function Login({setIsLoggedIn, isLoggedIn}) {
-
-  const navigate=useNavigate();
-  if(isLoggedIn){
-    navigate('/restaurants');
+function Login({ setIsLoggedIn, isLoggedIn }) {
+  const navigate = useNavigate();
+  if (isLoggedIn) {
+    navigate("/restaurants");
   }
 
-  const [formData, setFormData]=useState({
-    email:"", password:""
-  })
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-
-  function submitHandler(event){
-      event.preventDefault();
-      console.log("Login details",formData);
-      setIsLoggedIn(true);
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("Login details", formData);
+    setIsLoggedIn(true);
   }
 
-  function changeHandler(event){
-        
-        setFormData(
-            prevData =>{
-                return{
-                    ...prevData,              
-                    [event.target.name] : event.target.value
-                }
-            })  
+  function changeHandler(event) {
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
   return (
-    <div className='flex flex-col w-1/2 mx-auto  items-center p-10 pt-28 text-zinc-50 '>
-      <div className='p-4 my-2'>
-          <h1 className='text-2xl font-semibold'>Welcome Back!</h1>
-          <p className='text-xl italic font-extralight'>Khud ke sheher ke best restaurants dhoondein.</p>
-        </div>
-        <form onSubmit={submitHandler}
-        className=''
-        action="">
-          <label>
-            <p className='font-semibold mt-2'>E-mail Id</p>
-            <input onChange={changeHandler}
-            className='border-2 border-zinc-100 p-1 rounded-md w-full mt-1'
-            type="email" name="email" id="email" placeholder='Email ID' required/>
-          </label>
-          <label>
-            <p className='font-semibold mt-2'>Password</p>
-            <input onChange={changeHandler}
-            className='border-2 border-zinc-100 p-1 rounded-md w-full mt-1'
-            type="Password" name="password" id="Password" placeholder='Password' required/>
-          </label>
+    <div className="flex flex-col w-1/2 mx-auto  items-center p-10 pt-28 text-zinc-50 ">
+      <div className="p-4 my-2">
+        <h1 className="text-2xl font-semibold">Welcome Back!</h1>
+        <p className="text-xl italic font-extralight">
+          Khud ke sheher ke best restaurants dhoondein.
+        </p>
+      </div>
+      <form onSubmit={submitHandler} className="" action="">
+        <label>
+          <p className="font-semibold mt-2">E-mail Id</p>
+          <input
+            onChange={changeHandler}
+            className="border-2 border-zinc-100 p-1 rounded-md w-full mt-1"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email ID"
+            required
+          />
+        </label>
+        <label>
+          <p className="font-semibold mt-2">Password</p>
+          <input
+            onChange={changeHandler}
+            className="border-2 border-zinc-100 p-1 rounded-md w-full mt-1"
+            type="Password"
+            name="password"
+            id="Password"
+            placeholder="Password"
+            required
+          />
+        </label>
 
-          <button className='px-6 py-2 bg-zinc-900 rounded-md text-zinc-50 font-semibold my-4 cursor-pointer hover:bg-zinc-800'>Login</button>
-          <div className='link text-blue-500 underline font-semibold text-sm italic'>
-            <Link to="/signup">don't have account?</Link>
-          </div>
-        </form>
+        <button className="px-6 py-2 bg-zinc-900 rounded-md text-zinc-50 font-semibold my-4 cursor-pointer hover:bg-zinc-800">
+          Login
+        </button>
+        <div className="link text-blue-500 underline font-semibold text-sm italic">
+          <Link to="/signup">don't have account?</Link>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
